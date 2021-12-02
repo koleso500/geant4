ee7ab0c3e4a95f8b5ff78e2e9d08cb46206a6f3f
koleso500 <kolesnikovvasiliy5000@gmail.com>
2021-12-02 10:53:53 +0300

    2dec

--- project/src/DetectorConstruction.cc
+++ project/src/DetectorConstruction.cc
@@ -6,9 +6,6 @@
 #include "G4SystemOfUnits.hh"
 
 #include "G4Box.hh"
-#include "G4Cons.hh"
-#include "G4Sphere.hh"
-#include "G4Tubs.hh"
 #include "G4SubtractionSolid.hh"
 
 #include "G4LogicalVolume.hh"
@@ -28,16 +25,15 @@ DetectorConstruction::~DetectorConstruction()
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
-  /*G4Box* solidWorld =
-    new G4Box("World",
-       0.5 * world_size_x, 0.5 * world_size_y, 0.5 * world_size_z);*/
-  G4Sphere* solidWorld = new G4Sphere("World",
-                                  0,
-                                  world_radius,
-                                  0,
-                                  2*M_PI,
-                                  0,
-                                  2*M_PI);
+  G4NistManager* nist = G4NistManager::Instance();
+
+  G4double world_sizeX = 50*cm, world_sizeY = 30*cm, world_sizeZ = 30*cm;
+  
+  G4bool checkOverlaps = true; 
+  
+  G4Material* world_material = nist->FindOrBuildMaterial("G4_Galactic");
+
+  G4Box* solidWorld = new G4Box("World", world_sizeX, world_sizeY, world_sizeZ);
 
   G4LogicalVolume* logicWorld =
     new G4LogicalVolume(solidWorld,              // its solid
@@ -52,64 +48,15 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
                       0,                     //its mother  volume
                       false,                 //no boolean operation
                       0,                     //copy number
-                      false);                //overlaps checking
+                      checkOverlaps);         //overlaps checking
+  
+  G4double wide = 1*mm;
+  G4Material* plast_mat = nist->FindOrBuildMaterial("G4_Cu");
 
-// == == == == == == == == == == == == == == == ==
-// Создание анода
-// как вычитание из цилиндра наклонённого параллелипипеда
-//
-  auto cylinder = new G4Tubs("Cylinder",
-                          0.,
-                          anode_radius,
-                          anode_len,
-                          0.,
-                          2*M_PI);
-  auto box = new G4Box("box",
-                        anode_len*sin(abs(anode_angel)),
-                        anode_len*sin(abs(anode_angel)),
-                        anode_len*2);
-  auto rm = new G4RotationMatrix(0, -anode_angel, 0);
-  auto tm = new G4ThreeVector(0,0, -anode_len);
-  auto anode = new G4SubtractionSolid("anode", cylinder, box, rm, *tm);
-
-
-  G4LogicalVolume* logic = new G4LogicalVolume(anode, anode_material, "Anode");
-  new G4PVPlacement(
-    0,
-    G4ThreeVector(0,
-                  0,
-                  0),
-    logic,
-    "Anode",
-    logicWorld,
-    false,
-    0,
-    false);
-// == == == == == == == == == == == == == == == ==
-// Создание объёма детектора
-//
-  auto target = new G4Sphere("Target",
-                              world_radius - 0.01*cm,
-                              world_radius,
-                              0,
-                              2*M_PI,
-                              0,
-                              2*M_PI);
-
-
-  G4LogicalVolume* logic_tg = new G4LogicalVolume(target, target_material, "Target");
-  new G4PVPlacement(
-    0,
-    G4ThreeVector(0,
-                  0,
-                  0),
-    logic_tg,
-    "Target",
-    logicWorld,
-    false,
-    0,
-    false);
-
-
+  G4Box* plast = new G4Box("Plast", wide, 10*cm, 10*cm);
+  G4LogicalVolume* logicPlast = new G4LogicalVolume(plast, plast_mat, "Plast");
+  G4VPhysicalVolume* physPlast = new G4PVPlacement(0, G4ThreeVector(), logicPlast, "Plast", logicWorld, false, 0, checkOverlaps);
+  
   return physWorld;
+
 }
